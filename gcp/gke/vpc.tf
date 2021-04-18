6d508bc8f361960c9a2d606d803f45b81c4f5e13
mizu0 <mizu0@iMac.local>
2021-04-18 13:59:17 +0900

    add vpc.

---
+++ gcp/gke/vpc.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network" "gke_vpc" {
+    name = "gke-tutorial-vpc"
+    auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "gke_subnet" {
+    name = "gke-tutorial-subnet"
+    region = "us-east1"
+    network = google_compute_network.gke_vpc.name
+    ip_cidr_range = "10.10.0.0./24"
+}
